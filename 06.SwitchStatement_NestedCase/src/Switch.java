d765fddb7bef9e2b1d5f85d3400df0e40dfb1183
Akshat Kumar <akshatk1712@gmail.com>
2026-06-27 00:25:30 +0530

    fruit program added

--- 06.SwitchStatement_NestedCase/src/Switch.java
+++ 06.SwitchStatement_NestedCase/src/Switch.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a fruit: ");
         String fruit = in.next();
-
-         if (fruit.equals("mango")){
-             System.out.println("King of Fruit");
-         }
-        if (fruit.equals("apple")){
-            System.out.println("a red sweet Fruit");
+        switch (fruit.toLowerCase()) {
+            case "mango" -> System.out.println("king of the fruit");
+            case "apple" -> System.out.println("Red sweet fruit");
+            case "orange" -> System.out.println("round fruit");
+            case "grapes" -> System.out.println("Small fruit");
+            default -> System.out.println("Please enter a valid fruit");
         }
     }
 }

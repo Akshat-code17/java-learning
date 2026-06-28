f4388f0dc1d74a45e01ae8edf979c1214ea53c4e
Akshat Kumar <akshatk1712@gmail.com>
2026-06-28 21:16:09 +0530

    Adding Area of circle program

--- Kunal Kushwaha Assigments/Easy/03Conditions/Area Of Circle/src/program.java
+++ Kunal Kushwaha Assigments/Easy/03Conditions/Area Of Circle/src/program.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class program {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        System.out.println("Press 1 for Radius");
+        System.out.println("Press 2 for Diameter");
+        System.out.print("Your Choice: ");
+        int choice = in.nextInt();
+        double radius = 0;
+        if(choice == 1){
+            System.out.print("Enter the radius:");
+            radius = in.nextDouble();
+        }
+        else if(choice == 2){
+            System.out.print("Enter the diameter:");
+            radius = in.nextDouble()/2;
+        }
+        double Area = Math.PI * radius * radius;
+        System.out.println("Area =" + Area);
+
+        in.close();
 
     }
 }

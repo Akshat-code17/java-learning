7048eded3fc7fd08d98e73826387c547db2b1814
Akshat Kumar <akshatk1712@gmail.com>
2026-06-29 23:28:14 +0530

    area of rectangle program is added

--- Kunal Kushwaha Assigments/Easy/03Conditions/02.Area Of Triangle/src/program.java
+++ Kunal Kushwaha Assigments/Easy/03Conditions/02.Area Of Triangle/src/program.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Base:");
+        int base = in.nextInt();
+        System.out.print("Enter the Height:");
+        int height = in.nextInt();
+
+        if(base > 0 && height > 0){
+            double area = (base * height)/2.0 ;
+            System.out.println("Area of triangle:"+ area );
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+        in.close();
+
+    }
 }
---
+++ Kunal Kushwaha Assigments/Easy/03Conditions/03.Area Of Rectangle/src/program.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Length:");
+        float length = in.nextFloat();
+        System.out.print("Enter Breadth:");
+        float breadth = in.nextFloat();
+
+        if()
+
+        
+    }
+}

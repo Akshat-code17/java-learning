3a85f29c996f959bf4d878483c5f90ba3443d8de
Akshat Kumar <akshatk1712@gmail.com>
2026-06-29 23:34:10 +0530

    Error Fixed

--- Kunal Kushwaha Assigments/Easy/03Conditions/03.Area Of Rectangle/src/Program.java
+++ Kunal Kushwaha Assigments/Easy/03Conditions/03.Area Of Rectangle/src/Program.java
@@ -1,7 +1,6 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class program {
+public class Program {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Length:");
@@ -9,7 +8,14 @@ public class program {
         System.out.print("Enter Breadth:");
         float breadth = in.nextFloat();
 
-        if()
+        if(length > 0 && breadth > 0){
+            float area = (breadth * length) ;
+            System.out.println("Area of rectangle:"+ area );
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+        in.close();
 
         
     }

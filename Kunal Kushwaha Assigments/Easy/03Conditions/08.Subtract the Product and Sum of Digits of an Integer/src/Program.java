fe450c8efe722cfda936c241ec99310e7ba4fb50
Akshat Kumar <akshatk1712@gmail.com>
2026-07-01 23:37:09 +0530

    Added Subtract the Product and Sum of Digits of an Integer algorithm program

---
+++ Kunal Kushwaha Assigments/Easy/03Conditions/08.Subtract the Product and Sum of Digits of an Integer/src/Program.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = in.nextInt();
+
+        int sum = 0;
+        int product = 1;
+
+        // Your perfect logic
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+
+        int result = product - sum;
+        System.out.println("Result: " + result);
+
+        in.close();
+    }
+}

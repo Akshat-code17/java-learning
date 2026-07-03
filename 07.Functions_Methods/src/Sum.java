4f70fd13731ad0950e448ba2f436ccc76bd29a26
Akshat Kumar <akshatk1712@gmail.com>
2026-07-03 22:37:55 +0530

    returing the value__StringExample and Sum added

--- 07.Functions_Methods/src/Sum.java
+++ 07.Functions_Methods/src/Sum.java
@@ -2,10 +2,26 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
 
     }
 
+    // return the Value
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+
+        int num1 , num2 , sum;
+
+        System.out.print("Enter First number: ");
+        num1 = in.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        return sum;
+
+        // System.out.println("hello"); ..this will never execute
+    }
     static void sum(){
         Scanner in = new Scanner(System.in);
 

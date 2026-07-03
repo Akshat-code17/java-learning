670eaf75909d935a3759d85b9e69209029e56367
Akshat Kumar <akshatk1712@gmail.com>
2026-07-03 22:59:30 +0530

    Added swapping numbers code

---
+++ 07.Functions_Methods/src/Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 400;
+        int b = 50;
+
+        // Swapping Numbers
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + "  " + b);
+    }
+}

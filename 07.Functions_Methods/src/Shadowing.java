eb9686b6aff625c0551ef87c15d30afa7bb8576c
Akshat Kumar <akshatk1712@gmail.com>
2026-08-26 00:53:34 +0530

    completed the Shadowing

--- 07.Functions_Methods/src/Shadowing.java
+++ 07.Functions_Methods/src/Shadowing.java
@@ -1,6 +1,17 @@
 public class Shadowing {
-    public static void main() {
-        System.out.println();
+    static int x = 22; // this will be shadowed at line 8
+    public static void main(String[] args) {
+        System.out.println(x); // 22
+        int x; // the class variable at line 4 is shadowed by this
+//        System.out.println(x); // scope will begin when value is initialized
+        x = 40;
+        System.out.println(x); // 40
+        fun();
+
+    }
+
+    static void fun() {
+        System.out.println(x);
 
     }
 }

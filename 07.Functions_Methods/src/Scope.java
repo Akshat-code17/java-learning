ee3ea866a88ac5adebeb07b6ef3c3e7735a68d4e
Akshat Kumar <akshatk1712@gmail.com>
2026-08-25 23:37:10 +0530

    completed block Scope and scope

--- 07.Functions_Methods/src/Scope.java
+++ 07.Functions_Methods/src/Scope.java
@@ -1,5 +1,34 @@
 public class Scope {
     public static void main(String[] args) {
-        
+        int a = 20;
+        int b = 10;
+        String name = "rahul";
+
+        {
+//            int a = 67; // already initialized outside the block in the same block, hence you cannot initialize again.
+            a = 100;
+            name = "Kunal";
+            System.out.println(name);
+            System.out.println(a);
+            int c = 99;
+            // Values initialized in this block remain in this block
+        }
+        System.out.println(name);
+        System.out.println(a);
+//        System.out.println(c); // cannot be used outside the block
+
+        // scoping in for loops
+        for ( int i = 0; i < 4; i++){
+//            System.out.println(i);
+            int num = 90;
+            a = 10000000;
+        }
+        System.out.println(a);
+    }
+
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+        System.out.println(marks);
     }
 }
